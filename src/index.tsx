b48890ef2ffc1da6157c61929fb6a82e326ee186
Kier Borromeo <seraphipod@gmail.com>
2019-09-30 02:21:39 +0800

    Handle offset when hovering first/last

--- src/index.tsx
+++ src/index.tsx
@@ -8,22 +8,23 @@ import * as ReactDOM from 'react-dom'
 import thumbnail from './assets/thumbnail.jpg'
 
 const c = {
-  ITEMS_PER_ROW: 4,
+  ITEMS_PER_ROW: 8,
   SLIDE_WIDTH: 248,
   SLIDE_HOVER_WIDTH: 500,
   SLIDE_MARGIN: 4
 }
 
 const items = new Array(100).fill({
-  title: 'Pokemn Go'
+  title: 'Pokemon Go'
 })
 
 function App() {
   const [active, setActive] = useState(-1)
 
   const styles = useMemo(() => {
-    const activeRow = Math.ceil((active + 1) / c.ITEMS_PER_ROW)
-    
+    const activeRow = Math.ceil((active + 1) / c.ITEMS_PER_ROW) // Active item's index as row
+    const activeColumn = active % c.ITEMS_PER_ROW // Active item's index as column
+
     return items.map((_, i) => {
       const currentRow = Math.ceil((i + 1) / c.ITEMS_PER_ROW)
 
@@ -36,32 +37,43 @@ function App() {
 
       const scale = c.SLIDE_HOVER_WIDTH / c.SLIDE_WIDTH
       const translate = (c.SLIDE_HOVER_WIDTH - c.SLIDE_WIDTH) / 2
+      const offset = (() => {
+        if (activeColumn === 0) {
+          return (c.SLIDE_HOVER_WIDTH / 4)
+        }
+        
+        if (activeColumn === c.ITEMS_PER_ROW - 1) {
+          return -(c.SLIDE_HOVER_WIDTH / 4)
+        }
+        
+        return 0
+      })()
 
       if (active === i) {
         return {
           transition: '400ms all ease',
-          transform: `translateX(0) scale(${scale})`
+          transform: `translateX(${offset}px) scale(${scale})`
         }
       }
 
       if (i < active) {
         return {
           transition: '400ms all ease',
-          transform: `translateX(-${translate}px) scale(1)`
+          transform: `translateX(-${translate - offset}px) scale(1)`
         }
       }
 
       if (i > active) {
         return {
           transition: '400ms all ease',
-          transform: `translateX(${translate}px) scale(1)`
+          transform: `translateX(${translate + offset}px) scale(1)`
         }
       }
 
       return {}
     })
   }, [active])
-
+  
   return (
     <div className="app-body">
       <div className="app-container">
